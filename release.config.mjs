392b0344068ed6eb5ab5c2b25bebdaf664487139
Andrea Zaccheroni <andrea.zaccheroni@studio.unibo.it>
2026-09-16 00:23:10 +0200

    fix(ci): return to the original professor's template release setup...

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,18 @@
 let dryRun = (process.env.RELEASE_DRY_RUN || "false").toLowerCase() === "true";
+let testPypi = (process.env.RELEASE_TEST_PYPI || "false").toLowerCase() === "true";
+const pypiToken = process.env.PYPI_TOKEN;
 
-let prepareCmd = "poetry version -- \\${nextRelease.version} && poetry build";
+let prepareCmd = "poetry version -- \\${nextRelease.version}" + ` && poetry config pypi-token.pypi ${pypiToken}`;
+let publishCmd = `poetry publish --build`;
+
+if (testPypi) {
+    publishCmd += ` --repository testpypi`;
+    prepareCmd = prepareCmd.replace("pypi-token.pypi", "pypi-token.testpypi");
+}
+
+if (dryRun) {
+    publishCmd += " --dry-run";
+}
 
 import config from 'semantic-release-preconfigured-conventional-commits' with {type: 'json'};
 
@@ -9,6 +21,7 @@ config.tagFormat = "v${version}";
 config.plugins.push(
     ["@semantic-release/exec", {
         "prepareCmd" : prepareCmd,
+        "publishCmd": publishCmd,
     }]
 )
 
